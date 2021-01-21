3bb9287ebf092bd7339879b3f7ee1b2878accb54
raysalemi <ray@raysalemi.com>
2021-01-21 09:00:25 -0500

    Added simpleminded dut access example

---
+++ 37_Accessing_The_DUT/tinyalu_cocotb.py
@@ -0,0 +1,37 @@
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, RisingEdge
+import cocotb
+
+
+async def reset(dut):
+    await FallingEdge(dut.clk)
+    dut.reset_n = 0
+    dut.A = 0
+    dut.B = 0
+    dut.op = 0
+    await FallingEdge(dut.clk)
+    dut.reset_n = 1
+    await FallingEdge(dut.clk)
+
+
+@cocotb.test()
+async def test_alu(dut):
+    clock = Clock(dut.clk, 2, units="us")
+    cocotb.fork(clock.start())
+    await reset(dut)
+    await FallingEdge(dut.clk)
+    dut.A = 1
+    dut.B = 2
+    dut.op = 1 #add
+    dut.start = 1
+    await RisingEdge(dut.done)
+    await FallingEdge(dut.clk)
+    if dut.result == 3:
+        print("Passed")
+    else:
+        print("Failed")
+
+
+
+
+
