935ac95e15dd8b4d23c0e2be160b8097e698a650
raysalemi <ray@raysalemi.com>
2021-03-07 12:38:00 -0500

    Fixed up the PythonProxy

--- proxy_pkg.py
+++ proxy_pkg.py
@@ -10,39 +10,6 @@ class Ops(enum.IntEnum):
     XOR = 3
     MUL = 4
 
-class AluCommand(uvm_transaction):
-
-    def __init__(self, name, A=0, B=0, op=Ops.ADD):
-        super().__init__(name)
-        self.A = A
-        self.B = B
-        self.op = Ops(op)
-
-    def __eq__(self, other):
-        same = self.A == other.A \
-               and self.B == other.B \
-               and self.op == other.op
-        return same
-
-    def __str__(self):
-        return f"{self.get_name()} : A: 0x{self.A:02x} OP: {self.op.name} ({self.op.value}) B: 0x{self.B:02x}"
-    
-    def randomize(self):
-        self.A = random.randint(0,255)
-        self.B = random.randint(0,255)
-        self.op = random.choice(list(Ops))
-
-class AluResult(uvm_transaction):
-    def __init__(self, name, r):
-        super().__init__(name)
-        self.result = r
-
-    def __str__(self):
-        return f"{self.get_name()}: 0x{self.result:04x}"
-
-    def __eq__(self, other):
-        return self.result == other.result
-
 
 class PythonProxy(uvm_component):
     @staticmethod
@@ -101,10 +68,9 @@ class PythonProxy(uvm_component):
         return result        
         
     def run_phase(self):
-        while True:
+        while not ObjectionHandler().run_phase_complete():
             (aa, bb, op) = self.stim_get.get()
             result = self.alu_op(aa, bb, op)
-            result_txn = AluResult("result_txn", result)
             self.cmd_put.put((aa, bb, op))
             self.result_put.put(result)
             
