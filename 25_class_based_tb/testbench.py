8a85d670053d171f7da37afc25e589cd35615454
raysalemi <ray@raysalemi.com>
2021-09-21 08:01:32 -0400

    cleaning classes example

--- 25_class_based_tb/testbench.py
+++ 25_class_based_tb/testbench.py
@@ -1,18 +1,16 @@
 import cocotb
 import random
-import logging
 from pathlib import Path
 import sys
 # All testbenches use tinyalu_utils, so store it in a central
 # place and add its path to the sys path so we can import it
 sys.path.append(str(Path("..").resolve()))
-from tinyalu_utils import TinyAluBfm, Ops, alu_prediction  # noqa: E402
+from tinyalu_utils import TinyAluBfm, Ops, alu_prediction, logger  # noqa: E402
 
 
 class Tester():
-    def __init__(self, bfm, done):
+    def __init__(self, bfm):
         self.bfm = bfm
-        self.done = done
 
     async def execute(self):
         ops = list(Ops)
@@ -20,9 +18,10 @@ class Tester():
             aa = random.randint(0, 255)
             bb = random.randint(0, 255)
             await self.bfm.send_op(aa, bb, op)
-        await self.bfm.send_op(0, 0, 1)  # just to wait for the finish
-        await self.bfm.send_op(0, 0, 1)  # just to wait for the finish
-        self.done.set()
+        # send two dummy operations to allow
+        # last real operation to complete
+        await self.bfm.send_op(0, 0, 1)
+        await self.bfm.send_op(0, 0, 1)
 
 
 class Scoreboard():
@@ -31,8 +30,6 @@ class Scoreboard():
         self.ops = []
         self.results = []
         self.cvg = set()
-        self.logger = logging.getLogger()
-        self.logger.setLevel(logging.DEBUG)
 
     async def get_cmds(self):
         while True:
@@ -56,35 +53,29 @@ class Scoreboard():
             result = self.results.pop(0)
             pr = alu_prediction(aa, bb, Ops(op), error=False)
             if result == pr:
-                self.logger.info(f"PASSED: {aa} {op} {bb} = {result}")
+                logger.info(f"PASSED: {aa} {op} {bb} = {result}")
             else:
                 passed = False
-                self.logger.error(
+                logger.error(
                     f"FAILED: {aa} {op} {bb} = {result} - predicted {pr}")
 
         if len(set(Ops) - self.cvg) > 0:
-            self.logger.error(
+            logger.error(
                 f"Functional coverage error. Missed: {set(Ops)-self.cvg}")
             passed = False
         else:
-            self.logger.info("Covered all operations")
-
+            logger.info("Covered all operations")
         return passed
 
 
 @cocotb.test()
 async def test_alu(dut):
-    done = cocotb.triggers.Event()
-    logging.basicConfig(level=logging.NOTSET)
     bfm = TinyAluBfm(dut)
-    tester = Tester(bfm, done)
+    tester = Tester(bfm)
     scoreboard = Scoreboard(bfm)
     await bfm.reset()
-    cocotb.fork(bfm.driver_bfm())
-    cocotb.fork(bfm.result_mon_bfm())
-    cocotb.fork(bfm.cmd_mon_bfm())
-    cocotb.fork(tester.execute())
+    await bfm.start_bfms()
     cocotb.fork(scoreboard.execute())
-    await done.wait()
+    await tester.execute()
     passed = scoreboard.check_results()
     assert passed
