ded7c63ab9a5eeaf7f380902c560a440cedd86e1
raysalemi <ray@raysalemi.com>
2021-09-26 09:40:26 -0400

    uvm environment

--- 29_UVM_Environment/testbench.py
+++ 29_UVM_Environment/testbench.py
@@ -1,99 +1,144 @@
 import cocotb
 from pyuvm import *
 import random
+from pathlib import Path
+import sys
 # All testbenches use tinyalu_utils, so store it in a central
 # place and add its path to the sys path so we can import it
-import sys
-from pathlib import Path
 sys.path.append(str(Path("..").resolve()))
-from tinyalu_utils import TinyAluBfm, Ops, alu_prediction  # noqa: E402
+from tinyalu_utils import TinyAluBfm, Ops, alu_prediction, logger  # noqa: E402
 
 
-class BaseEnv(uvm_env):
-    async def set_up_sim(self):
-        cocotb.fork(self.bfm.driver_bfm())
-        cocotb.fork(self.bfm.result_mon_bfm())
-        cocotb.fork(self.bfm.cmd_mon_bfm())
-        await self.bfm.reset()
+class Tester(uvm_component):
 
     def build_phase(self):
         self.bfm = ConfigDB().get(self, "", "BFM")
 
-    def end_of_elaboration_phase(self):
-        self.cvg = set()
-
-    def extract_phase(self):
-        self.missed_ops = set(Ops) - self.cvg
-
-    def check_phase(self):
-        if len(self.missed_ops) > 0:
-            self.logger.warning(f"Functional coverage error. Missed: {set(Ops)-self.cvg}")
-
-
-class RandomEnv(BaseEnv):
     async def run_phase(self):
         self.raise_objection()
-        await self.set_up_sim()
-        for op in list(Ops):
-            A = random.randrange(256)
-            B = random.randrange(256)
-            self.cvg.add(op)
-            predicted_result = alu_prediction(A, B, op)
-            await self.bfm.send_op(A, B, int(op))
-            actual_result = await self.bfm.get_result()
-            if predicted_result == actual_result:
-                self.logger.info(f"PASSED: {A:02x} {op.name} {B:02x} = {actual_result:04x}")
-            else:
-                self.logger.error(f"FAILED: {A:02x} {op.name} {B:02x} = {actual_result:04x} expected {predicted_result:04x}")
-        self.drop_objection()  # drop the objection to end
-
-
-class MaxEnv(BaseEnv):
-    async def run_phase(self):
-        self.raise_objection()
-        await self.set_up_sim()
-        A = 0xFF
-        B = 0xFF
-        for op in list(Ops):
-            self.cvg.add(op)
-            predicted_result = alu_prediction(A, B, op)
-            await self.bfm.send_op(A, B, int(op))
-            actual_result = await self.bfm.get_result()
-            if predicted_result == actual_result:
-                self.logger.info(f"PASSED: {A:02x} {op.name} {B:02x} = {actual_result:04x}")
-            else:
-                self.logger.error(f"FAILED: {A:02x} {op.name} {B:02x} = {actual_result:04x} expected {predicted_result:04x}")
+        await self.bfm.reset()
+        await self.bfm.start_bfms()
+        ops = list(Ops)
+        for op in ops:
+            aa = random.randint(0, 255)
+            bb = random.randint(0, 255)
+            await self.bfm.send_op(aa, bb, op)
+        # send two dummy operations to allow
+        # last real operation to complete
+        await self.bfm.send_op(0, 0, 1)
+        await self.bfm.send_op(0, 0, 1)
         self.drop_objection()
 
 
-class RandomTest(uvm_test):
+class MaxTester(Tester):
+
+    async def run_phase(self):
+        self.raise_objection()
+        await self.bfm.reset()
+        await self.bfm.start_bfms()
+        ops = list(Ops)
+        for op in ops:
+            aa = 0xFF
+            bb = 0xFF
+            await self.bfm.send_op(aa, bb, op)
+        # send two dummy operations to allow
+        # last real operation to complete
+        await self.bfm.send_op(0, 0, 1)
+        await self.bfm.send_op(0, 0, 1)
+        self.drop_objection()
+
+
+class Scoreboard(uvm_component):
+
     def build_phase(self):
-        self.env = RandomEnv("env", self)
+        self.bfm = ConfigDB().get(self, "", "BFM")
+        self.cmds = []
+        self.results = []
+        self.cvg = set()
+
+    async def get_cmds(self):
+        while True:
+            cmd = await self.bfm.get_cmd()
+            self.cmds.append(cmd)
+
+    async def get_results(self):
+        while True:
+            result = await self.bfm.get_result()
+            self.results.append(result)
+
+    async def run_phase(self):
+        cocotb.fork(self.get_cmds())
+        cocotb.fork(self.get_results())
+
+    def check_phase(self):
+        passed = True
+        for cmd in self.cmds:
+            (aa, bb, op) = cmd
+            self.cvg.add(Ops(op))
+            actual = self.results.pop(0)
+            prediction = alu_prediction(aa, bb, Ops(op))
+            if actual == prediction:
+                logger.info(f"PASSED: {aa} {Ops(op).name} {bb} = {actual}")
+            else:
+                passed = False
+                logger.error(
+                    f"FAILED: {aa} {Ops(op).name} {bb} = {actual} - predicted {prediction}")
+
+        if len(set(Ops) - self.cvg) > 0:
+            logger.error(
+                f"Functional coverage error. Missed: {set(Ops)-self.cvg}")
+            passed = False
+        else:
+            logger.info("Covered all operations")
+        assert passed
+
+
+class BaseAluEnv(uvm_env):
+    """Instantiate the BFM and scoreboard"""
+
+    def build_phase(self):
+        dut = ConfigDB().get(self, "", "DUT")
+        bfm = TinyAluBfm(dut)
+        ConfigDB().set(None, "*", "BFM", bfm)
+        self.scoreboard = Scoreboard("scoreboard", self)
+
+class RandomAluEnv(BaseAluEnv):
+    """Generate random operands"""
+
+    def build_phase(self):
+        super().build_phase()
+        self.tester = Tester("tester", self)
+
+
+class MaxAluEnv(BaseAluEnv):
+    """Generate maximum operands"""
+
+    def build_phase(self):
+        super().build_phase()
+        self.tester = MaxTester("tester", self)
+
+
+class RandomTest(uvm_test):
+    """Run with random operands"""
+    def build_phase(self):
+        self.env = RandomAluEnv("env", self)
 
 
 class MaxTest(uvm_test):
+    """Run with max operands"""
     def build_phase(self):
-        self.env = MaxEnv("env", self)
+        self.env = MaxAluEnv("env", self)
 
 
 @cocotb.test()
-async def random_env(dut):
-    """
-    Demonstrates using an environment
-    """
-    bfm = TinyAluBfm(dut)
-    ConfigDB().set(None, "*", "BFM", bfm)
+async def random_test(dut):
+    """Random operands"""
+    ConfigDB().set(None, "*", "DUT", dut)
     await uvm_root().run_test("RandomTest")
-    assert True
 
 
 @cocotb.test()
-async def max_env(dut):
-    """
-    Demonstrates using an environment to run a different test
-    """
-    bfm = TinyAluBfm(dut)
-    await bfm.start_bfms()
-    ConfigDB().set(None, "*", "BFM", bfm)
+async def max_test(dut):
+    """Maximum operands"""
+    ConfigDB().set(None, "*", "DUT", dut)
     await uvm_root().run_test("MaxTest")
-    assert True
