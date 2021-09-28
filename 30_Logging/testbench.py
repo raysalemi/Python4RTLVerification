8ede6772c78f519d34e4bacd54a8ecd28332cc45
raysalemi <ray@raysalemi.com>
2021-09-28 07:30:34 -0400

    Fixed logging example

--- 30_Logging/testbench.py
+++ 30_Logging/testbench.py
@@ -1,7 +1,6 @@
 import cocotb
 from pyuvm import *
-
-# Basic Logging
+import logging
 
 
 class LogComp(uvm_component):
@@ -12,7 +11,6 @@ class LogComp(uvm_component):
         self.logger.warning("This is warning")
         self.logger.error("This is error")
         self.logger.critical("This is critical")
-        self.logger.log(FIFO_DEBUG, "This is a FIFO message")
         self.drop_objection()
 
 
@@ -26,83 +24,26 @@ class DebugTest(LogTest):
         self.set_logging_level_hier(DEBUG)
 
 
-class FifoDebugTest(LogTest):
+class FileTest(LogTest):
     def end_of_elaboration_phase(self):
-        self.set_logging_level_hier(DEBUG)
-
-
-
-@cocotb.test()
-async def logging_example(dut):
-    """Demonstrate basic logging"""
-    await uvm_root().run_test("LogTest")
-    assert True
-
-
-# Set logging levels
-
-
-class Producer(uvm_component):
-    def build_phase(self):
-        self.pp = uvm_put_port("pp", self)
-
-    async def run_phase(self):
-        self.raise_objection()
-        for nn in range(5):
-            await self.pp.put(nn)
-            print(f"Put {nn}", end=" ")
-        self.drop_objection()
-
-
-class Consumer(uvm_component):
-    def build_phase(self):
-        self.gp = uvm_get_port("gp", self)
-
-    async def run_phase(self):
-        while True:
-            nn = await self.gp.get()
-            print(f"Got {nn}", end=" ")
-
-
-class FIFOTest(uvm_test):
-    def build_phase(self):
-        self.prod = Producer("prod", self)
-        self.cons = Consumer("cons", self)
-        self.fifo = uvm_tlm_fifo("fifo", self)
-
-    def end_of_elaboration_phase(self):
-        self.set_logging_level_hier(FIFO_DEBUG)
-
-    def connect_phase(self):
-        self.prod.pp.connect(self.fifo.put_export)
-        self.cons.gp.connect(self.fifo.get_export)
-
-
-@cocotb.test()
-async def logging_levels(dut):
-    """Demonstrate logging levels"""
-    await uvm_root().run_test("FIFOTest")
-    assert True
-
-
-class FileFIFOTest(uvm_test):
-    def build_phase(self):
-        self.prod = Producer("prod", self)
-        self.cons = Consumer("cons", self)
-        self.fifo = uvm_tlm_fifo("fifo", self)
-
-    def end_of_elaboration_phase(self):
-        file_handler = logging.FileHandler("put.txt")
+        file_handler = logging.FileHandler("log.txt", mode="w")
         self.add_logging_handler_hier(file_handler)
-        self.set_logging_level_hier(FIFO_DEBUG)
-
-    def connect_phase(self):
-        self.prod.pp.connect(self.fifo.put_export)
-        self.cons.gp.connect(self.fifo.get_export)
+        self.remove_streaming_handler_hier()
 
 
 @cocotb.test()
-async def logging_to_a_file(dut):
-    """Demonstrate logging to a file"""
-    await uvm_root().run_test("FileFIFOTest")
-    assert True
+async def default_logging_levels(_):
+    """Demonstrate default logging levels"""
+    await uvm_root().run_test("LogTest")
+
+
+@cocotb.test()
+async def debug_logging_level(_):
+    """Demonstrate debug logging level"""
+    await uvm_root().run_test("DebugTest")
+
+
+@cocotb.test()
+async def log_to_file(_):
+    """Write log messages to file"""
+    await uvm_root().run_test("FileTest")
