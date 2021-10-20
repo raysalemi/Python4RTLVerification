ca6cb55c4de51e8f405707f8f3a0b18b102c64b5
raysalemi <ray@raysalemi.com>
2021-10-20 08:54:06 -0400

    cleaner config example

--- 31_The_configuration_database/testbench.py
+++ 31_The_configuration_database/testbench.py
@@ -2,65 +2,36 @@ import cocotb
 from pyuvm import *
 
 
-class NameMyself(uvm_component):
+class MsgLogger(uvm_component):
+
     def build_phase(self):
-        if self.get_name() == "aa":
-            self.cc = NameMyself("cc", self)
-
-    def end_of_elaboration_phase(self):
-        print("My name:", self.get_full_name())
-
-
-class NameTest(uvm_test):
-    def build_phase(self):
-        self.aa = NameMyself("aa", self)
-        self.bb = NameMyself("bb", self)
+        self.msg = ConfigDB().get(self, "", "MSG")
 
     async def run_phase(self):
         self.raise_objection()
-        print("Top's name:", self.get_full_name())
+        self.logger.info(self.msg)
         self.drop_objection()
+
+
+class MsgEnv(uvm_env):
+    def build_phase(self):
+        self.loga = MsgLogger("loga", self)
+        self.logb = MsgLogger("logb", self)
+
+
+class LogTest(uvm_test):
+
+    def build_phase(self):
+        self.env = MsgEnv("env", self)
+        ConfigDB().set(self, "env.loga", "MSG", "loga msg")
+        ConfigDB().set(self, "env.logb", "MSG", "logb msg")
+
+
 
 
 @cocotb.test()
 async def name_myself(dut):
     """Exercise hierarchy naming"""
-    await uvm_root().run_test("NameTest")
+    await uvm_root().run_test("LogTest")
     assert True
 
-
-# ConfigDB Example
-
-
-class ConfigNameMyself(uvm_component):
-    def build_phase(self):
-        try:
-            build_child = ConfigDB().get(self, "", "CHILD")
-        except UVMConfigItemNotFound:
-            build_child = False
-        if build_child:
-            self.cc = ConfigNameMyself("cc", self)
-
-    def end_of_elaboration_phase(self):
-        print("My name:", self.get_full_name())
-
-
-class ConfigTest(uvm_test):
-    def build_phase(self):
-        ConfigDB().set(self, "aa", "CHILD", False)
-        ConfigDB().set(self, "bb", "CHILD", True)
-        print("--- ConfigDB()----", ConfigDB(), "\n", "-" * 18)
-        self.aa = ConfigNameMyself("aa", self)
-        self.bb = ConfigNameMyself("bb", self)
-
-    async def run_phase(self):
-        self.raise_objection()
-        print("Top's name:", self.get_full_name())
-        self.drop_objection()
-
-
-@cocotb.test()
-async def config_play(dut):
-    """Demonstrate using the ConfigDB()"""
-    await uvm_root().run_test("ConfigTest")
-    assert True
