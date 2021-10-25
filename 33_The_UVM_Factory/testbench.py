3072cc927fd7431a73c6e359479a3054b04f50fd
raysalemi <ray@raysalemi.com>
2021-10-25 09:27:17 -0400

    Now with new simpler factory examples

--- 33_The_UVM_Factory/testbench.py
+++ 33_The_UVM_Factory/testbench.py
@@ -9,6 +9,83 @@ sys.path.append(str(Path("..").resolve()))
 from tinyalu_utils import TinyAluBfm, Ops, alu_prediction, logger  # noqa: E402
 
 
+class TinyComponent(uvm_component):
+    async def run_phase(self):
+        self.raise_objection()
+        self.logger.info("I'm so tiny!")
+        self.drop_objection()
+
+
+class TinyTest(uvm_test):
+    def build_phase(self):
+        self.tc = TinyComponent("tc", self)
+
+
+@cocotb.test()
+async def tiny_test(_):
+    await uvm_root().run_test("TinyTest")
+
+
+class TinyFactoryTest(uvm_test):
+    def build_phase(self):
+        self.tc = TinyComponent.create("tc", self)
+
+
+@cocotb.test()
+async def tiny_factory_test(_):
+    await uvm_root().run_test("TinyFactoryTest")
+
+
+class MediumComponent(uvm_component):
+    async def run_phase(self):
+        self.raise_objection()
+        self.logger.info("I'm medium size.")
+        self.drop_objection()
+
+
+class MediumFactoryTest(TinyFactoryTest):
+    def build_phase(self):
+        uvm_factory().set_type_override_by_type(
+            TinyComponent, MediumComponent)
+        super().build_phase()
+
+
+@cocotb.test()
+async def medium_factory_test(_):
+    await uvm_root().run_test("MediumFactoryTest")
+
+
+class MediumNameTest(TinyFactoryTest):
+    def build_phase(self):
+        uvm_factory().set_type_override_by_name(
+            "TinyComponent", "MediumComponent")
+        super().build_phase()
+
+
+@cocotb.test()
+async def medium_name_test(_):
+    await uvm_root().run_test("MediumNameTest")
+
+
+class TwoCompEnv(uvm_env):
+    def build_phase(self):
+        self.tc1 = TinyComponent.create("tc1", self)
+        self.tc2 = TinyComponent.create("tc2", self)
+
+
+class TwoCompTest(uvm_test):
+    def build_phase(self):
+        uvm_factory().set_inst_override_by_type(
+            TinyComponent, MediumComponent, "uvm_test_top.env.tc1")
+        self.env = TwoCompEnv("env", self)
+
+
+@cocotb.test()
+async def two_comp_test(_):
+    uvm_factory().clear_overrides()
+    await uvm_root().run_test("TwoCompTest")
+
+
 class Tester(uvm_component):
 
     def build_phase(self):
