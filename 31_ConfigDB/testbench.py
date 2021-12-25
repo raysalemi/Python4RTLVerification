75248c777e2c1430e8ebf36eed9efcf04a45cb1a
Ray Salemi <ray@raysalemi.com>
2021-12-25 11:57:10 -0500

    latest confidb

--- 31_ConfigDB/testbench.py
+++ 31_ConfigDB/testbench.py
@@ -2,16 +2,18 @@ import cocotb
 from pyuvm import *
 
 
-class MsgLogger(uvm_component):
+#  ## The ConfigDB().get method
 
-    def build_phase(self):
-        self.msg = ConfigDB().get(self, "", "MSG")
+class MsgLogger(uvm_component):
 
     async def run_phase(self):
         self.raise_objection()
-        self.logger.info(self.msg)
+        msg = ConfigDB().get(self, "", "MSG")
+        self.logger.info(msg)
         self.drop_objection()
 
+
+# ## The ConfigDB().set() method
 
 class MsgEnv(uvm_env):
     def build_phase(self):
@@ -19,16 +21,74 @@ class MsgEnv(uvm_env):
         self.logb = MsgLogger("logb", self)
 
 
-class LogTest(uvm_test):
+class MsgTest(uvm_test):
 
     def build_phase(self):
         self.env = MsgEnv("env", self)
-        ConfigDB().set(self, "env.loga", "MSG", "loga msg")
-        ConfigDB().set(self, "env.logb", "MSG", "logb msg")
+        ConfigDB().set(self, "env.loga", "MSG", "LOG A msg")
+        ConfigDB().set(self, "env.logb", "MSG", "LOG B msg")
 
 
 @cocotb.test()
 async def log_msgs(dut):
-    """Exercise hierarchy storage"""
-    await uvm_root().run_test("LogTest")
-    assert True
+    await uvm_root().run_test(MsgTest)
+
+
+# ## Wildcards
+class MultiMsgEnv(MsgEnv):
+    def build_phase(self):
+        self.talka = MsgLogger("talka", self)
+        self.talkb = MsgLogger("talkb", self)
+        super().build_phase()
+
+
+class MultiMsgTest(uvm_test):
+    def build_phase(self):
+        self.env = MultiMsgEnv("env", self)
+        ConfigDB().set(self, "env.loga", "MSG", "LOG A msg")
+        ConfigDB().set(self, "env.logb", "MSG", "LOG B msg")
+        ConfigDB().set(self, "env.t*", "MSG", "TALK TALK")
+
+
+@cocotb.test()
+async def multi_msg(_):
+    await uvm_root().run_test(MultiMsgTest)
+
+
+# ## Global data
+class GlobalEnv(MultiMsgEnv):
+    def build_phase(self):
+        self.gtalk = MsgLogger("gtalk", self)
+        super().build_phase()
+
+
+class GlobalTest(uvm_test):
+    def build_phase(self):
+        self.env = GlobalEnv("env", self)
+        ConfigDB().set(self, "env.loga", "MSG", "LOG A msg")
+        ConfigDB().set(self, "env.logb", "MSG", "LOG B msg")
+        ConfigDB().set(self, "env.t*", "MSG", "TALK TALK")
+        ConfigDB().set(None, "*", "MSG", "GLOBAL")
+
+
+@cocotb.test()
+async def global_msg(_):
+    await uvm_root().run_test(GlobalTest)
+
+
+# ## Parent/child conflict
+class ConflictEnv(uvm_env):
+    def build_phase(self):
+        self.loga = MsgLogger("loga", self)
+        ConfigDB().set(self, "loga", "MSG", "CHILD RULES!")
+
+
+class ConflictTest(uvm_test):
+    def build_phase(self):
+        self.env = ConflictEnv("env", self)
+        ConfigDB().set(self, "env.loga", "MSG", "PARENT RULES!")
+
+
+@cocotb.test()
+async def conflict_test(_):
+    await uvm_root().run_test(ConflictTest)
